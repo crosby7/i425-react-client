5970ac1c8a61aa023c7f39904b608effa99c5282
crosby7 <53411767+crosby7@users.noreply.github.com>
2025-11-07 14:23:24 -0500

    Initialize React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Course Project Client App</h1>
+        <h2>Using Freshmart API</h2>
+    </>
+  )
+}
+
+export default App
