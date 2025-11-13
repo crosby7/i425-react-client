9370f8814e1ca97eccaa1eae761e886e61524781
Cameron <camcrosby09@gmail.com>
2025-11-13 10:11:22 -0500

    Update signout.jsx

--- src/pages/auth/signout.jsx
+++ src/pages/auth/signout.jsx
@@ -1,29 +1,24 @@
-import {useAuth} from "../../services/useAuth";
-import {useEffect} from 'react';
-
-
+import { useAuth } from "../../services/useAuth";
+import { useEffect } from "react";
 
 const Signout = () => {
-    const {logout} = useAuth();
+  const { logout } = useAuth();
 
-    //need to wrap the setState call inside useEffect.
-    useEffect(() => {
-        logout();
-    })
+  //need to wrap the setState call inside useEffect.
+  useEffect(() => {
+    logout();
+  });
 
-    return (
-        <>
-            <div className="main-heading">
-                <div className="container">Authorization</div>
-            </div>
-            <div className="sub-heading">
-                <div className="container">Sign Out</div>
-            </div>
-            <div className="main-content container">
-                Thank you for your visit. You have signed out.
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="main-heading">
+        <div className="container">Sign Out</div>
+      </div>
+      <div className="main-content container">
+        Thank you for your visit. You have signed out.
+      </div>
+    </>
+  );
 };
 
 export default Signout;
